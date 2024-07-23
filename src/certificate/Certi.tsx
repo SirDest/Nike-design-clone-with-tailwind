8aafc19789598629052928ee8e6a0aa1dca7205e
Akinro Destined <104671422+SirDest@users.noreply.github.com>
2024-07-23 14:20:09 +0100

    certificate

--- src/certificate/Certi.tsx
+++ src/certificate/Certi.tsx
@@ -1,14 +1,9 @@
 import React, { useRef, useState } from "react";
 
 import jsPDF from "jspdf";
-import "jspdf/dist/polyfills.es.js";
-// import certificateImg from "../../src/static/images/certificate.jpeg";
-import bg from "../../src/static/images/WhatsApp Image 2024-07-22 at 09.51.04 - Copy.jpeg";
+import bg from "../../src/static/images/Certficate.jpg";
 
 const Certi = () => {
-  // const [name, setName] = useState("");
-  // const [course, setCourse] = useState("");
-
   const reportTemplateRef = useRef<HTMLDivElement>(null);
 
   const handleGeneratePdf = () => {
@@ -25,30 +20,41 @@ const Certi = () => {
         callback: function (doc) {
           doc.save("certificate.pdf");
         },
-        y: 5, // Adjust position from the top the image
+        y: 7, // Adjust position from the top the image
       });
     } else {
       doc.save("certificate.pdf");
     }
   };
 
-  const today = new Date();
+  const todayDate = new Date();
 
-  const options: Intl.DateTimeFormatOptions = {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  };
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
-  const formattedDate = today
-    .toLocaleDateString("en-US", options)
-    .replace(",", "");
+  const day = todayDate.getDate();
+  const month = months[todayDate.getMonth()];
+  const year = todayDate.getFullYear();
 
   return (
-    <div className='w-full h-screen bg-blue-400 flex flex-col'>
+    <div className='w-full h-fit flex flex-col'>
       <div ref={reportTemplateRef} className='w-[630px] relative'>
-        <div className='absolute w-fit h-[150px] top-28 left-11 flex flex-col gap-2'>
-          <p className='text-[10px] font-semibold'>{formattedDate}</p>
+        <div className='absolute w-[fit] h-[150px] top-28 left-11 flex flex-col gap-2'>
+          <p className='text-[10px] font-semibold'>
+            {month} {day}, {year}
+          </p>
           <p className='font-bold text-[22px]'>Godwin, Odira Frontend</p>
           <p className='text-[10px] font-semibold'>
             has successfully completed
@@ -61,10 +67,21 @@ const Certi = () => {
             environment to hone your skills.
           </p>
         </div>
+        <div className='absolute w-fit h-fit bottom-10 right-16 flex flex-col text-right text-[9px] font-semibold'>
+          <p>Verified at:</p>
+          <a href='/' className='text-blue-700 m-0 p-0'>
+            http://training.outcess.com/verify/1234566
+          </a>
+          <p>Outcess has confirmed the identity of this individual and has</p>
+          <p>participated in the course</p>
+        </div>
+
         <img src={bg} alt='certificate' />
       </div>
-
-      <button className='button' onClick={handleGeneratePdf}>
+      <button
+        className='bg-green-400 p-4 w-fit m-auto'
+        onClick={handleGeneratePdf}
+      >
         Download Certificate
       </button>
     </div>
